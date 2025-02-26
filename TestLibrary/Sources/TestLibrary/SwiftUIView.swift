954cd5c5af53299f5dac64aaa299c57a4b379403
Shota <sh.klibadze@gmail.com>
2025-02-26 18:55:47 +0400

    first commit

---
+++ TestLibrary/Sources/TestLibrary/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  TestLibrary
+//
+//  Created by Shotiko Klibadze on 26.02.25.
+//
+
+import SwiftUI
+
+struct WidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetView()
+}
