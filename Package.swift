48ed4575e6430a6ad538e043abdd931e32fbaa7c
Shota <sh.klibadze@gmail.com>
2025-02-26 19:16:35 +0400

    lin

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestLibrary",
+    name: "Lib",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "TestLibrary",
-            targets: ["TestLibrary"]),
+            name: "Lib",
+            targets: ["Lib"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TestLibrary"),
+            name: "Lib"),
 
     ]
 )
